9fd2753a6cee5b933375bc4e83bdaf3c8d22a410
Andrea Detassis <chebelom@users.noreply.github.com>
2024-10-15 12:39:18 +0200

    ssh keys, take 1

--- 2_First_target/output.tf
+++ 2_First_target/output.tf
@@ -12,3 +12,11 @@ output "ssh_connect_alias" {
 output "ssh_connect_target-id" {
   value = "boundary connect ssh -target-id ${boundary_target.aws_linux_private.id}"
 }
+
+output "ssh-key-private" {
+  value = tls_private_key.rsa_4096_key.private_key_pem
+}
+
+output "ssh-key-public" {
+  value = tls_private_key.rsa_4096_key.public_key_openssh
+}
