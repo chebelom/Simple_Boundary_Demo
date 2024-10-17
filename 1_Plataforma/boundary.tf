1c539a948776ff4c824eeef180e184fbf81342d6
Andrea Detassis <chebelom@users.noreply.github.com>
2024-10-17 15:29:40 +0200

    ade, take 5

--- 1_Plataforma/boundary.tf
+++ 1_Plataforma/boundary.tf
@@ -1,6 +1,6 @@
 resource "hcp_boundary_cluster" "boundary" {
   cluster_id = var.boundary_cluster_id
-  username   = var.username
-  password   = var.password
+  username   = var.boundary_username
+  password   = var.boundary_password
   tier       = var.boundary_tier
 }
