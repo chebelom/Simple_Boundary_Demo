78a3ed5c18593fc3e1503f0cc918a5fd5a78d9dd
Andrea Detassis <chebelom@users.noreply.github.com>
2024-10-17 11:16:27 +0200

    try check block

--- 2_First_target/aws-static-target.tf
+++ 2_First_target/aws-static-target.tf
@@ -97,13 +97,6 @@ resource "aws_security_group" "public_network_boundary_ssh" {
     Name = "allow_ssh"
   }  
 }
-check "certificate" {
-  assert {
-    condition     = contains(["0.0.0.0/0"], aws_security_group.public_network_boundary_ssh.ingress["cidr_blocks"]) 
-    error_message = "SSH is open to the world!!1!"
-  }
-}
-
 
 resource "aws_instance" "boundary_target" {
   #count                  = 1
