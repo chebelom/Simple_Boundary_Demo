7a8339f4dc9a1f0c327fc64b88b0fdb10f1c79fe
Andrea Detassis <chebelom@users.noreply.github.com>
2024-10-18 13:55:59 +0200

    ade, take 44

--- 6_Multi_hop/main.tf
+++ 6_Multi_hop/main.tf
@@ -23,44 +23,54 @@ terraform {
 }
 
 # Declare the provider for the AWS resource to be managed by Terraform
+provider "doormat" {}
+
+data "doormat_aws_credentials" "creds" {
+  provider = doormat
+  role_arn = "arn:aws:iam::${var.aws_account_id}:role/tfc-doormat-role_6-multi-hop"
+}
+
+# Declare the provider for the AWS resource to be managed by Terraform
 provider "aws" {
-  region = var.region
+  region     = var.region
+  access_key = data.doormat_aws_credentials.creds.access_key
+  secret_key = data.doormat_aws_credentials.creds.secret_key
+  token      = data.doormat_aws_credentials.creds.token
 }
 
 # Declare the provider for the HashiCorp Boundary resource to be managed by Terraform
 provider "boundary" {
   # Use variables to provide values for the provider configuration
-  addr                   = ""
-  auth_method_id         = var.authmethod
-  auth_method_login_name = var.username
-  auth_method_password   = var.password
+  addr                   = data.tfe_outputs.platform.values.boundary_public_url
+  auth_method_login_name = var.boundary_username
+  auth_method_password   = var.boundary_password
 }
 
 provider "vault" {
-  address = data.tfe_outputs.platform.values.vault_public_url
-  # token     = var.vault_token
+  address   = data.tfe_outputs.platform.values.vault_public_url
   namespace = "admin" # Set for HCP Vault
+  token     = data.tfe_outputs.platform.values.vault_token
 }
 
 # Remote Backend to obtain VPC details 
 data "tfe_outputs" "platform" {
-  organization = "hashicorp-italy"
-  workspace = "1_Platform"
+  organization = var.tfc_organization
+  workspace    = "1_Platform"
 }
 
 data "tfe_outputs" "first-target-2" {
-  organization = "hashicorp-italy"
-  workspace = "2_First_target"
+  organization = var.tfc_organization
+  workspace    = "2_first-target"
 }
 
-# Remote Backend to obtain Vault Token 
+# # Remote Backend to obtain Vault Token 
 data "tfe_outputs" "vault-config-4" {
-  organization = "hashicorp-italy"
-  workspace = "4_Vault_SSH_Injection-vault-config"
+  organization = var.tfc_organization
+  workspace    = "4-injection"
 }
 
 # Remote Backend to obtain Vault Token 
 data "tfe_outputs" "self-managed-5" {
-  organization = "hashicorp-italy"
-  workspace = "5_Self_Managed_Worker"
+  organization = var.tfc_organization
+  workspace    = "5-self-managed"
 }
--- 6_Multi_hop/variables.tf
+++ 6_Multi_hop/variables.tf
@@ -1,16 +1,14 @@
-variable "username" {
+variable "boundary_username" {
   type = string
 }
 
-variable "password" {
+variable "boundary_password" {
   type = string
 }
-
 
 variable "region" {
   description = "The region of the HCP HVN and Vault cluster."
   type        = string
-  default     = "eu-west-2"
 }
 
 variable "key_pair_name" {
@@ -22,11 +20,19 @@ variable "authmethod" {
 }
 
 variable "scenario5_ssh_alias" {
-  type        = string
+  type    = string
   default = "scenario5.ssh.injected.boundary.demo"
 }
 
 variable "scenario5_rdp_alias" {
-  type        = string
+  type    = string
   default = "scenario5.rdp.broker.boundary.demo"
+}
+
+variable "aws_account_id" {
+  type = string
+}
+
+variable "tfc_organization" {
+  type = string
 }
