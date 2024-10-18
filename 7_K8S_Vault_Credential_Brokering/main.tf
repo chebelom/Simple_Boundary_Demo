406dde9e46c0e04002cd7a7723ef38ddba7e489e
Andrea Detassis <chebelom@users.noreply.github.com>
2024-10-18 14:14:41 +0200

    ade, take 49

--- 7_K8S_Vault_Credential_Brokering/main.tf
+++ 7_K8S_Vault_Credential_Brokering/main.tf
@@ -43,7 +43,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", data.tfe_outputs.eks.values.cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
     command     = "aws"
   }
 }
