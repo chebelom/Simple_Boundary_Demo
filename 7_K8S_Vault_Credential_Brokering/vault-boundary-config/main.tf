4b96a1b947bed86e1940bac966f91105b401e306
Andrea Detassis <chebelom@users.noreply.github.com>
2024-10-18 17:21:00 +0200

    ade, take 62

--- 7_K8S_Vault_Credential_Brokering/vault-boundary-config/main.tf
+++ 7_K8S_Vault_Credential_Brokering/vault-boundary-config/main.tf
@@ -9,6 +9,10 @@ terraform {
       source  = "hashicorp/vault"
       version = "3.17.0"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.33.0"
+    }
     doormat = {
       source  = "doormat.hashicorp.services/hashicorp-security/doormat"
       version = "~> 0.0.13"
