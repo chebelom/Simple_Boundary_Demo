d0f5824af07fb4e01bb16eab1a05fcbc31ed274b
Andrea Detassis <chebelom@users.noreply.github.com>
2024-11-05 13:19:16 +0100

    Add psql port

--- components/networking/network.tf
+++ components/networking/network.tf
@@ -150,7 +150,32 @@ resource "aws_security_group" "privatesg" {
   }
 }
 
+resource "aws_security_group" "allow_vault_egress_ingress" {
+  name        = "allow_vault_egress_ingress"
+  description = "Allow Vault outbound traffic and some ingress"
+  vpc_id      = aws_vpc.peer.id
 
+  egress {
+    from_port   = 8200
+    to_port     = 8200
+    protocol    = "tcp"
+    cidr_blocks = ["172.25.16.0/20"]
+  }
+  # Allow connection to postgres from Vault
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["172.25.16.0/20"]
+  }
+  # Allow LDAP from Vault to VPC
+  ingress {
+    from_port   = 389
+    to_port     = 389
+    protocol    = "tcp"
+    cidr_blocks = ["172.25.16.0/20"]
+  }
+}
 
 
 # # data "aws_internet_gateway" "default" {
