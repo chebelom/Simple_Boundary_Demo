d8dfbff44909e1509efed4785a911c702cfe0ee5
Andrea Detassis <chebelom@users.noreply.github.com>
2024-12-16 11:11:56 +0100

    add hcp sec group to public

--- components/networking/network.tf
+++ components/networking/network.tf
@@ -124,7 +124,7 @@ resource "aws_security_group" "publicsg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0", var.hvn_cidr_block] 
   }
   
   egress {
