6ceda0354630e42f4b86309ec9f201b47d3de196
Andrea Detassis <chebelom@users.noreply.github.com>
2024-12-18 09:38:38 +0100

    get hcp token

--- components/configs-boundary/main.tf
+++ components/configs-boundary/main.tf
@@ -13,7 +13,7 @@ resource "boundary_scope" "project" {
   auto_create_default_role = true
 }
 
-resource "hcp_vault_cluster_admin_token" "token" {
+resource "hcp_vault_cluster_admin_token" "admin" {
   cluster_id = var.vault_cluster_id
 }
 
@@ -21,8 +21,7 @@ resource "boundary_credential_store_vault" "vault" {
   name        = "vault"
   description = "My Vault credential store!"
   address     = var.boundary_address
-  token       = hcp_vault_cluster_admin_token.token.token
+  token       = hcp_vault_cluster_admin_token.admin.token
   scope_id    = boundary_scope.project.id
   namespace   = "admin"
-  depends_on = [ hcp_vault_cluster_admin_token.token ]
 }
