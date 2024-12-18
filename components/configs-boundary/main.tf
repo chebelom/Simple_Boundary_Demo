0923be0337f7af61948250540e60e1b8899f6508
Andrea Detassis <chebelom@users.noreply.github.com>
2024-12-18 10:43:13 +0100

    readd boundary with no HCP provider

--- components/configs-boundary/main.tf
+++ components/configs-boundary/main.tf
@@ -13,10 +13,6 @@ resource "boundary_scope" "project" {
   auto_create_default_role = true
 }
 
-resource "hcp_vault_cluster_admin_token" "admin" {
-  cluster_id = var.vault_cluster_id
-}
-
 resource "boundary_credential_store_vault" "vault" {
   name        = "vault"
   description = "My Vault credential store!"
