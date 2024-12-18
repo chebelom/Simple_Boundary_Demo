f4b415ce7c5533035fce6932e6e7355684c1a0d0
Andrea Detassis <chebelom@users.noreply.github.com>
2024-12-18 10:01:17 +0100

    maybe using the vault address could help, take 2

--- components/configs-boundary/main.tf
+++ components/configs-boundary/main.tf
@@ -13,15 +13,16 @@ resource "boundary_scope" "project" {
   auto_create_default_role = true
 }
 
-resource "hcp_vault_cluster_admin_token" "admin" {
-  cluster_id = var.vault_cluster_id
-}
+# resource "hcp_vault_cluster_admin_token" "admin" {
+#   cluster_id = var.vault_cluster_id
+# }
 
 resource "boundary_credential_store_vault" "vault" {
   name        = "vault"
   description = "My Vault credential store!"
   address     = var.vault_address
-  token       = hcp_vault_cluster_admin_token.admin.token
+#   token       = hcp_vault_cluster_admin_token.admin.token
+  token       = var.boundary_vault_token
   scope_id    = boundary_scope.project.id
   namespace   = "admin"
 }
